c4f61524984772feb61d0018629fa0e1026106f6
adryan <dryan_guns@yahoo.co.id>
2020-11-12 23:57:27 +0700

    updates

---
+++ pages/dashboard.vue
@@ -0,0 +1,329 @@
+<template>
+    <!--
+  Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
+  Read the documentation to get started: https://tailwindui.com/documentation
+-->
+<div>
+<div v-if="this.loading" class="loading-page">
+    <p>Loading...</p>
+</div>
+<nav v-else class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <!-- Mobile menu button-->
+        <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
+          <!-- Icon when menu is closed. -->
+          <!--
+            Heroicon name: menu
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+          <!-- Icon when menu is open. -->
+          <!--
+            Heroicon name: x
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+          <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex-shrink-0">
+          <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/v1/workflow-mark-on-dark.svg" alt="Workflow logo">
+          <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/v1/workflow-logo-on-dark.svg" alt="Workflow logo">
+        </div>
+        <div class="hidden sm:block sm:ml-6">
+          <div class="flex">
+            <a href="#" class="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
+            welcome {{this.name}}
+            </a>
+           
+          </div>
+        </div>
+      </div>
+      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out" aria-label="Notifications" v-on:click="clickLogout">
+          <!-- Logout -->
+          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+         </svg>
+        </button>
+
+        <!-- Profile dropdown -->
+        <div class="ml-3 relative">
+          <div>
+            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
+              <img class="h-8 w-8 rounded-full" src="https://avatars1.githubusercontent.com/u/11289176?s=460&u=742d904835c5ff776446a7390ce1653fd5f4c89b&v=4" alt="">
+            </button>
+          </div>
+          <!--
+            Profile dropdown panel, show/hide based on dropdown state.
+
+            Entering: "transition ease-out duration-100"
+              From: "transform opacity-0 scale-95"
+              To: "transform opacity-100 scale-100"
+            Leaving: "transition ease-in duration-75"
+              From: "transform opacity-100 scale-100"
+              To: "transform opacity-0 scale-95"
+          -->
+        
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--
+    Mobile menu, toggle classes based on menu state.
+
+    Menu open: "block", Menu closed: "hidden"
+  -->
+  <div class="hidden sm:hidden">
+    <div class="px-2 pt-2 pb-3">
+      <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
+      <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
+      <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
+      <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+    </div>
+  </div>
+</nav>
+
+
+<!-- body -->
+<div class="flex flex-col">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <table class="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Title
+              </th>
+              <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Date
+              </th>
+              <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Rate
+              </th>
+              <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                ID
+              </th>
+         
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+     
+            <tr v-for="(item, index) in this.movie" :key="index" v-on:click="clickDetail(item)">
+            
+              <td class="px-6 py-4 whitespace-no-wrap">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full" :src="'http://image.tmdb.org/t/p/w185' + item.poster_path" alt="">
+                  </div>
+                  <div class="ml-4">
+                    <div class="text-sm leading-5 font-medium text-gray-900">
+                       {{item.title}}
+                    </div>
+                   
+                  </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-no-wrap">
+                <div class="text-sm leading-5 text-gray-900"> 
+                {{item.release_date}}</div>
+                
+              </td>
+              <td class="px-6 py-4 whitespace-no-wrap">
+                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                 {{item.vote_average}}
+                </span>
+              </td>
+              <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                 {{item.id}}
+              </td>
+        
+            </tr>
+
+            <!-- More rows... -->
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- modal -->
+<div v-show="this.modal" class="fixed z-10 inset-0 overflow-y-auto">
+  <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+    <div class="fixed inset-0 transition-opacity">
+      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    </div>
+
+    <!-- This element is to trick the browser into centering the modal contents. -->
+    <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
+    <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    -->
+    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start">
+        
+
+          <img :src="movieDetail.img">
+          
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+              {{movieDetail.title}} <br />
+              <span class="text-sm leading-5 text-gray-700">
+               {{movieDetail.date}} 
+              </span> <br />
+              <span class="text-sm leading-5 text-gray-700">
+               {{movieDetail.genre}} 
+              </span>
+            </h3>
+            <div class="mt-2">
+              <p class="text-sm leading-5 text-gray-700">
+                {{movieDetail.overview}}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <span v-on:click="clickDetail" class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+          <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+            Close
+          </button>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</template>
+
+<script>
+// import Tables from "~components/tables";
+export default {
+    head: {
+        title: 'Dashboard'
+    },
+    // components: {
+    //   Tables: ('~/components/Tables')
+    // }
+    data() {
+      return {
+        name: '',
+        loading: true,
+        modal: false,
+        movie: [],
+        movieDetail: {
+          title: '',
+          genre: '',
+          date: '',
+          overview: '',
+          img: ''
+        }
+      }
+    },
+    methods: {
+      clickLogout(e) {
+        e.preventDefault();
+        window.sessionStorage.removeItem('email')
+        window.sessionStorage.removeItem('statusLogin')
+        this.$router.push('/')
+      },
+      getMovie() {
+        fetch(
+            'https://api.themoviedb.org/3/movie/top_rated?api_key=bf1b09065cc06d4ff6e4ede4601348ff&language=en-US&page=1'
+          ).then(res => res.json())
+          .then(res => {
+             console.log('rr' , res)
+             this.movie = res.results
+             this.loading = false
+          })  
+      },
+      clickDetail(item) {
+        if (this.modal) {
+          this.modal = false
+          this.movieDetail.title = ''
+          this.movieDetail.date = ''
+          this.movieDetail.overview = ''
+          this.movieDetail.genre = ''
+          this.movieDetail.img = ''
+        } else {
+          this.modal = true
+
+          fetch(
+            `https://api.themoviedb.org/3/movie/${item.id}?api_key=bf1b09065cc06d4ff6e4ede4601348ff&language=en-US`
+          ).then(res => res.json())
+          .then(res => {
+            console.log('detail' , res)
+            this.movieDetail.title = res.title
+            this.movieDetail.date = res.release_date
+            this.movieDetail.overview = item.overview
+            this.movieDetail.genre = res.genres.map(a => a.name).join(', ')
+            this.movieDetail.img = 'http://image.tmdb.org/t/p/w185' + item.poster_path
+  
+          })  
+
+          
+        }
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.$nuxt.$loading.start()
+        setTimeout(() => this.$nuxt.$loading.finish(), 500)
+      })
+
+      if (window.sessionStorage.getItem('statusLogin') !== 'true') {
+          this.$router.push('/')
+      } else {
+          this.getMovie()
+          this.name = window.sessionStorage.getItem('email')
+      }
+    }
+}
+
+</script>
+
+<style>
+ .loading-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.8);
+    text-align: center;
+    padding-top: 200px;
+    font-size: 30px;
+    font-family: sans-serif;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,98 @@
 <template>
-  <div class="container">
-    <NLink to="/login">
-      About Page
-    </NLink>
-  </div>
+     <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full">
+            <div>
+            <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/v1/workflow-mark-on-white.svg" alt="Workflow">
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+                Sign in to your account
+            </h2>
+            <p class="mt-2 text-center text-sm leading-5 text-gray-600">
+                
+            </p>
+            </div>
+            <form class="mt-8" action="#" method="POST">
+            <input type="hidden" name="remember" value="true">
+            <div class="rounded-md shadow-sm">
+                <div>
+                <input v-model="formUsername" aria-label="User Name" name="text" type="text" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="User Name">
+                </div>
+                <div class="-mt-px">
+                <input v-model="formPassword" aria-label="Password" name="password" type="password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Password">
+                </div>
+            </div>
+
+            <div class="mt-6 flex items-center justify-between">
+                <div class="flex items-center text-sm err-msg">
+                
+                </div>
+
+                <div class="text-sm leading-12">
+                <div class="flex items-center text-sm err-msg">
+                    {{this.formError}}
+                </div>
+                </div>
+            </div>
+
+            <div class="mt-6">
+                <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out" v-on:click="clickLogin">
+                <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+                Sign in
+                </button>
+                </div>
+            </form>
+        </div>
+       
+        </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+<script>
+export default {
+    head: {
+        title: 'Login Page'
+    },
+    data() {
+      return {
+        mountains: [],
+        formError: null,
+        formUsername: '',
+        formPassword: ''
+      }
+    },
+    methods: {
+        clickLogin (e) {
+          e.preventDefault();  
+          this.mountains = fetch(
+            'https://api.themoviedb.org/3/movie/latest?api_key=bf1b09065cc06d4ff6e4ede4601348ff&language=en-US'
+            ).then(res => res.json())
+            try {
+                if (this.formUsername === '' && this.formPassword === '') {
+                   this.formError = 'Masukkan password & email anda'
+
+                } else if (this.formUsername === '' && this.formPassword !== '') {
+                   this.formError = 'Masukkan email anda'
+                } else if (this.formUsername !== '' && this.formPassword === '') {
+                   this.formError = 'Masukkan password anda'
+                } else {
+                   window.sessionStorage.setItem('statusLogin', 'true')
+                   window.sessionStorage.setItem('email', this.formUsername)
+                   this.formError = ''
+                   this.$router.push('/dashboard')
+                }                
+            } catch (e) {
+                this.formError = e.message
+            }
+        }
+    },
+    mounted() {
+        if (window.sessionStorage.getItem('statusLogin') === 'true') {
+            this.$router.push('/dashboard')
+        }
+    },
+}
 </script>
 
 <style>
@@ -17,42 +101,9 @@ export default Vue.extend({})
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.err-msg {
+  font-size: 12px;
+  color: red;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
